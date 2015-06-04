eb940f52dcb5dcb677acec360edfdc0aed086cb8
Jonathan Harris <jonathan@marginal.org.uk>
2015-06-04 18:48:52 +0100

    Add UTC timezone to timestamps in BPC and CSV output.

--- bpc.py
+++ bpc.py
@@ -14,7 +14,7 @@ def export(data, csv=False):
 
     filename = join(config.get('outdir'), '%s.%s.%s.%s' % (data['lastSystem']['name'].strip(), data['lastStarport']['name'].strip(), time.strftime('%Y-%m-%dT%H.%M.%S', time.localtime(querytime)), csv and 'csv' or 'bpc'))
 
-    timestamp = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(querytime))
+    timestamp = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(querytime))
     if csv:
         header = 'System;Station;Commodity;Sell;Buy;Demand;;Supply;;Date;\n'
         rowheader = '%s;%s' % (data['lastSystem']['name'].strip(), data['lastStarport']['name'].strip())
