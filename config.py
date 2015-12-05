ae0a919027918438da9116491304ca2b66cc7866
Jonathan Harris <jonathan@marginal.org.uk>
2015-12-05 13:35:04 +0000

    Release 1.88

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from sys import platform
 appname = 'EDMarketConnector'
 applongname = 'E:D Market Connector'
 appcmdname = 'EDMC'
-appversion = '1.8.7.0'
+appversion = '1.8.8.1'
 
 
 if platform=='darwin':
