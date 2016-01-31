da87eb71e6280a01b10e50c7d69d643943a2d609
Jonathan Harris <jonathan@marginal.org.uk>
2016-01-31 12:44:39 +0000

    Fix race condition on stopping.

--- hotkey.py
+++ hotkey.py
@@ -296,10 +296,11 @@ elif platform == 'win32':
                 self.thread.start()
 
         def unregister(self):
-            if self.thread:
-                PostThreadMessage(self.thread.ident, WM_QUIT, 0, 0)
-                self.thread.join()	# Wait for it to unregister hotkey and quit
-            self.thread = None
+            thread = self.thread
+            if thread:
+                self.thread = None
+                PostThreadMessage(thread.ident, WM_QUIT, 0, 0)
+                thread.join()	# Wait for it to unregister hotkey and quit
 
         def worker(self, keycode, modifiers):
 
