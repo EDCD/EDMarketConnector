31af8be0483a7adf84bf996309745001048b361f
AnthorNet <anthor.net@gmail.com>
2016-10-26 14:05:11 +0200

    Add shipId / Credits from LoadGame event

--- monitor.py
+++ monitor.py
@@ -77,10 +77,12 @@ class EDLogs(FileSystemEventHandler):
         self.is_beta = False
         self.mode = None
         self.cmdr = None
+        self.shipId = None
         self.system = None
         self.station = None
         self.coordinates = None
         self.ranks = None
+        self.credits = None
 
     def set_callback(self, name, callback):
         if name in self.callbacks:
@@ -218,8 +220,10 @@ class EDLogs(FileSystemEventHandler):
                 self.ranks = None
             elif entry['event'] == 'LoadGame':
                 self.cmdr = entry['Commander']
+                self.shipId = entry['ShipID']
                 self.mode = entry.get('GameMode')	# 'Open', 'Solo', 'Group', or None for CQC
                 self.ranks = { "Combat": None, "Trade": None, "Explore": None, "Empire": None, "Federation": None, "CQC": None }
+                self.credits = { "balance": entry['Credits'], "loan": entry['Loan'] }
             elif entry['event'] == 'NewCommander':
                 self.cmdr = entry['Name']
             elif entry['event'] in ['Undocked']:
