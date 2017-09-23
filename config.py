d1442a96ca4853ffc28dfa3e8ffb018168bafbd7
Jonathan Harris <jonathan@marginal.org.uk>
2017-09-23 12:33:32 +0100

    Release 2.43

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from sys import platform
 appname = 'EDMarketConnector'
 applongname = 'E:D Market Connector'
 appcmdname = 'EDMC'
-appversion = '2.4.2.0'
+appversion = '2.4.3.0'
 
 update_feed = 'https://marginal.org.uk/edmarketconnector.xml'
 update_interval = 47*60*60
