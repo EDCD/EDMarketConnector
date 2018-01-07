EDCD/EDMarketConnector

commit d763e5931a10bfe66ea6ebad7214d99e1b08db89

Correct config examples
config.get() never returns int for consistency across platforms.
Fixes #271

config.py
@@ -149,7 +149,7 @@ class Config:
             if hasattr(val, '__iter__'):
                 return list(val)	# make writeable
             else:
-                return val
+                return unicode(val)
 
         def getint(self, key):
             try:
@@ -233,7 +233,7 @@ class Config:
             elif typ.value == REG_MULTI_SZ:
                 return [x for x in ctypes.wstring_at(buf, len(buf)-2).split(u'\x00')]
             else:
-                return buf.value
+                return unicode(buf.value)
 
         def getint(self, key):
             typ  = DWORD()

commit 17f89afa0d230595171a94e90e092aaf448a6173

Return None for unset config entries on OSX
Was broken by d763e59. Fixes #281

config.py
@@ -146,7 +146,9 @@ class Config:
 
         def get(self, key):
             val = self.settings.get(key)
-            if hasattr(val, '__iter__'):
+            if val is None:
+                return None
+            elif hasattr(val, '__iter__'):
                 return list(val)	# make writeable
             else:
                 return unicode(val)