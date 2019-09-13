3900e3e4ab9a986a9917c1c21cfc6803f815e9a1
Athanasius <github@miggy.org>
2019-09-13 16:52:43 +0100

    protocol.py: De-future

--- protocol.py
+++ protocol.py
@@ -1,10 +1,6 @@
-from __future__ import print_function
 # edmc: protocol handler for cAPI authorisation
 
 
-from future import standard_library
-standard_library.install_aliases()
-from builtins import object
 import threading
 import urllib.request, urllib.error, urllib.parse
 import sys
