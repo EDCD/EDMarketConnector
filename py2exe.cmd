5246bcd794bf96f9b9b67f495e75e3befab73930
Jonathan Harris <jonathan@marginal.org.uk>
2019-09-30 03:08:07 +0100

    Packaging cleanups

--- py2exe.cmd
+++ py2exe.cmd
@@ -1,2 +1,2 @@
 @REM http://www.py2exe.org/index.cgi/OptimizedBytecode
-"C:\Program Files (x86)\Python27\python.exe" -OO setup.py py2exe
+"C:\Program Files (x86)\Python37-32\python.exe" -OO setup.py py2exe
