c19a1244ee1d38d8aba1f0002620d7ce861ba16b
Bernd Gollesch <bgollesch@speed.at>
2020-05-25 12:29:55 +0200

    set carrier version

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from sys import platform
 appname = 'EDMarketConnector'
 applongname = 'E:D Market Connector'
 appcmdname = 'EDMC'
-appversion = '3.5.0.0-bgol'
+appversion = '3.5.0.0-bgol+carrier'
 
 update_feed = 'https://marginal.org.uk/edmarketconnector.xml'
 update_interval = 47*60*60
