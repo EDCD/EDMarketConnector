a9dc5e5153d099ac31cdf1252fea493084c245e4
A_D <aunderscored@gmail.com>
2020-10-18 08:51:34 +0200

    Added hooks into killswitches in plugins

--- plugins/eddn.py
+++ plugins/eddn.py
@@ -10,12 +10,13 @@ from collections import OrderedDict
 from os import SEEK_SET
 from os.path import join
 from platform import system
-from typing import TYPE_CHECKING, Any, AnyStr, Dict, Iterator, List, Mapping, MutableMapping, Optional, Tuple
+from typing import TYPE_CHECKING, Any, AnyStr, Dict, Iterator, List, Mapping, MutableMapping, Optional
 from typing import OrderedDict as OrderedDictT
-from typing import Sequence, TextIO
+from typing import Sequence, TextIO, Tuple
 
 import requests
 
+import killswitch
 import myNotebook as nb  # noqa: N813
 from companion import CAPIData, category_map
 from config import applongname, appversion, config
@@ -25,7 +26,7 @@ from prefs import prefsVersion
 from ttkHyperlinkLabel import HyperlinkLabel
 
 if sys.platform != 'win32':
-    from fcntl import lockf, LOCK_EX, LOCK_NB
+    from fcntl import LOCK_EX, LOCK_NB, lockf
 
 
 if TYPE_CHECKING:
@@ -126,6 +127,10 @@ class EDDN:
         :param cmdr: the CMDR to use as the uploader ID
         :param msg: the payload to send
         """
+        if killswitch.is_disabled('plugins.eddn_send'):
+            logger.warning("eddn.send has been disabled via killswitch. Returning")
+            return
+
         uploader_id = cmdr
 
         to_send: OrderedDictT[str, str] = OrderedDict([
