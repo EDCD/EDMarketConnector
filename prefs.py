c7c1a311b828650d3f47759fd396cf1907ac4419
Athanasius <github@miggy.org>
2021-01-12 15:38:07 +0000

    prefs: Avoid Tk event_generate() calls during shutdown

--- prefs.py
+++ prefs.py
@@ -1203,4 +1203,5 @@ class PreferencesDialog(tk.Toplevel):
             except Exception:
                 AXIsProcessTrustedWithOptions({kAXTrustedCheckOptionPrompt: True})
 
-            self.parent.event_generate('<<Quit>>', when="tail")
+            if not config.shutting_down:
+                self.parent.event_generate('<<Quit>>', when="tail")
