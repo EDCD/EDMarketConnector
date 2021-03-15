97e7c9087f84ca1015b1d2c3909424d97e5f9de5
A_D <aunderscored@gmail.com>
2021-03-15 19:08:18 +0200

    Updated event classes

--- plugin/event.py
+++ plugin/event.py
@@ -1,11 +1,35 @@
 
 import time
+from typing import Any, Optional
 
 
-class Event:
-    def __init__(self, name: str, event_time: None) -> None:
-        self.name = ""
-        self.time = time.time()
+class BaseEvent:
+    """
+    Base Event class.
 
-        if event_time is not None:
-            self.time = event_time
+    Intended to simply signify that something happened. If you want to pass data
+    with your event, use one of the subclasses below.
+    """
+
+    def __init__(self, name: str, event_time: float = None) -> None:
+        self.name = name
+        if event_time is None:
+            event_time = time.time()
+
+        self.time = event_time
+
+
+class BaseDataEvent(BaseEvent):
+    """
+    Base Data carrying event class.
+
+    Same as BaseEvent but carries some data as well.
+    """
+
+    def __init__(self, name: str, data: Any = None, event_time: float = None) -> None:
+        super().__init__(name, event_time=event_time)
+        self.data = data
+
+
+class JournalEvent(BaseDataEvent):
+    """Journal event."""
