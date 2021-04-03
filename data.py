b93868008a0200595ef646b7ee7ea8d92119697f
Athanasius <github@miggy.org>
2021-04-03 10:17:37 +0100

    Move commodity.py bracketmap into data.py

--- data.py
+++ data.py
@@ -1,1 +1,13 @@
-"""Static data."""
+"""
+Static data.
+
+For easy reference any variable should be prefixed with the name of the file it
+was either in originally, or where the primary code utilising it is.
+"""
+
+commodity_bracketmap = {
+    0: '',
+    1: 'Low',
+    2: 'Med',
+    3: 'High',
+}
