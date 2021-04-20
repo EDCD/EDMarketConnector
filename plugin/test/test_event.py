7492bbda71fdd9d55d986d635f0bd151f9e65d10
A_D <aunderscored@gmail.com>
2021-04-20 06:59:03 +0200

    added test for method with multiple callbacks

--- plugin/test/test_event.py
+++ plugin/test/test_event.py
@@ -31,3 +31,20 @@ def test_catchall_event(plugin_manager: PluginManager) -> None:
 
     # if called exists, assert that it has the expected content.
     assert all(test_event in getattr(p, 'called') if hasattr(p, 'called') else True for p in loaded)
+
+
+def test_multiple_hooks(plugin_manager: PluginManager) -> None:
+    """Test that a hook function with multiple defined callbacks works."""
+    p = plugin_manager.load_plugin(good_path / 'multi_callback')
+    assert p is not None
+
+    test_journal = BaseEvent('core.journal_event')
+    test_not_journal = BaseEvent('uncore.not_journal_event')
+
+    p.fire_event(test_journal)
+    assert len(getattr(p.plugin, 'called')) == 1
+    p.fire_event(test_not_journal)
+    assert len(getattr(p.plugin, 'called')) == 2
+
+    assert test_journal in getattr(p.plugin, 'called')
+    assert test_not_journal in getattr(p.plugin, 'called')
