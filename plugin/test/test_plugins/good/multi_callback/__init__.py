b3d701eed3f202fc708847065bc88c87bf7a744b
A_D <aunderscored@gmail.com>
2021-04-20 06:59:03 +0200

    added test for method with multiple callbacks

---
+++ plugin/test/test_plugins/good/multi_callback/__init__.py
@@ -0,0 +1,30 @@
+"""Test plugin that loads correctly."""
+import semantic_version
+
+from plugin.decorators import edmc_plugin, hook
+from plugin.plugin import Plugin
+from plugin.event import BaseEvent
+from plugin.plugin_info import PluginInfo
+
+
+@edmc_plugin
+class GoodPlugin(Plugin):
+    """Plugin that loads correctly."""
+
+    def load(self) -> PluginInfo:
+        """Nothing Special."""
+        self.called: list[BaseEvent] = []
+
+        return PluginInfo(
+            name="good",
+            version=semantic_version.Version.coerce('0.0.1'),
+            authors=['A_D']
+        )
+
+    @hook('core.journal_event')
+    @hook('uncore.not_journal_event')
+    def multiple_things(self, e: BaseEvent):
+        """Multiple hooks on one method."""
+        self.called.append(e)
+
+    print(id(multiple_things))
