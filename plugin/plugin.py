6a2fefd572c6b9bca15f21182ee59c1e6c10598c
A_D <aunderscored@gmail.com>
2021-04-30 13:31:24 +0200

    Started removing needed plugin imports

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -1,15 +1,94 @@
 """
 EDMC specific plugin implementations.
 
-See _plugin.py for base plugin implementation.
+See base_plugin.py for base plugin implementation.
 
-_plugin.py and plugin.py are distinct to allow for simpler testing -- this
+base_plugin.py and plugin.py are distinct to allow for simpler testing -- this
 file imports many different chunks of EDMC that are not needed for testing of the plugin system itself.
 """
 from __future__ import annotations
 
+import pathlib
+from typing import TYPE_CHECKING, Optional, final
+
+import config
+import constants
+import l10n
 from plugin.base_plugin import BasePlugin
+from theme import _Theme, theme
+
+if TYPE_CHECKING:
+    import semantic_version
+
+    from EDMCLogging import LoggerMixin
+    from plugin.manager import PluginManager
 
 
 class EDMCPlugin(BasePlugin):
     """Elite Dangerous Market Connector plugin base."""
+
+    def __init__(self, logger: LoggerMixin, manager: PluginManager, path: pathlib.Path) -> None:
+        super().__init__(logger, manager, path)
+
+    @final
+    def translate(self, s: str, context: Optional[str] = None) -> str:
+        """
+        Translate the given string.
+
+        :param s: String to translate
+        :param context: Context to find the translation files, defaults to the plugins directory
+        :return: The translated string
+        """
+        if context is None:
+            context = str(self.path)
+
+        return l10n.Translations.translate(s, context=context)
+
+    @final
+    def show_error(self, msg: str) -> None:
+        """
+        Show an error on the UI and log it.
+
+        :param msg: The message to show
+        """
+        self.log.error(msg)
+        raise NotImplementedError
+
+    # Properties for accessing various bits of EDMC data
+
+    @property
+    @final
+    def theme(self) -> _Theme:
+        """Theming for plugin widgets."""
+        return theme
+
+    @property
+    @final
+    def edmc_name(self) -> str:
+        """EDMC appname."""
+        return constants.appname
+
+    @property
+    @final
+    def edmc_long_name(self) -> str:
+        """EDMC applongname."""
+        return constants.applongname
+
+    @property
+    @final
+    def edmc_cmd_name(self) -> str:
+        """EDMC cmdname."""
+        return config.appcmdname
+
+    @final
+    def edmc_version(self, no_build=False) -> semantic_version.Version:
+        """Return the current EDMC Version."""
+        if no_build:
+            return config.appversion_nobuild()
+        return config.appversion()
+
+    @property
+    @final
+    def edmc_copyright(self) -> str:
+        """Return the current EDMC Copyright statement."""
+        return config.copyright
