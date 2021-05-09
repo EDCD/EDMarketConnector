bf265d0750f435a0693322aa037ae2f21d1e452e
A_D <aunderscored@gmail.com>
2021-05-09 16:43:39 +0200

    Allowed string versions on PluginInfo

--- plugin/plugin_info.py
+++ plugin/plugin_info.py
@@ -1,7 +1,7 @@
 """Information on a given plugin."""
 
 import dataclasses
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import semantic_version
 
@@ -11,9 +11,14 @@ class PluginInfo:
     """PluginInfo holds information about a loaded plugin."""
 
     name: str
-    version: semantic_version.Version
+    version: Union[semantic_version.Version, str]
     authors: Optional[List[str]] = None
     comment: Optional[str] = None
 
     # TODO: implement update checking and optional downloading
     update_url: Optional[str] = None
+
+    def __post_init__(self):
+        """Post-init to convert a string self.version to a Version."""
+        if isinstance(self.version, str):
+            self.version = semantic_version.Version.coerce(self.version)
