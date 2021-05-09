ad1c83cccf05fe53fecfd4430b17ad20361a31d1
A_D <aunderscored@gmail.com>
2021-05-09 16:42:45 +0200

    Added ease of use getitem and get methods to DictDataEvent

--- plugin/event.py
+++ plugin/event.py
@@ -31,7 +31,20 @@ class BaseDataEvent(BaseEvent):
         self.data = data
 
 
-class JournalEvent(BaseDataEvent):
+class DictDataEvent(BaseDataEvent):
+    """Same as a data event, but promises data is a dict."""
+
+    def __init__(self, name: str, data: dict[Any, Any], event_time: float) -> None:
+        super().__init__(name, data=data, event_time=event_time)
+        self.data: dict[Any, Any] = data
+
+        self.get = self.data.get
+
+    def __getitem__(self, name: str) -> Any:
+        return self.data[name]
+
+
+class JournalEvent(DictDataEvent):
     """Journal event."""
 
     def __init__(
@@ -39,10 +52,17 @@ class JournalEvent(BaseDataEvent):
         system: Optional[str], station: Optional[str], state: Dict[str, Any]
     ) -> None:
 
-        self.data: Dict[str, Any]  # Override the definition in BaseDataEvent to be more specific
+        self.data: dict[str, Any]  # Override the definition in BaseDataEvent to be more specific
         super().__init__(name, data=data, event_time=event_time)
         self.commander = cmdr
         self.is_beta = is_beta
         self.system = system
         self.station = station
         self.state = state
+
+        self.get = self.data.get  # Ease of use wrapper
+
+    @property
+    def event_name(self) -> str:
+        """Get the event name for the current event."""
+        return self.data['event']
