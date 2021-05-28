6de13b9c4d701f07003feeb6473221bce7634b7d
A_D <aunderscored@gmail.com>
2021-05-28 14:54:51 +0200

    Re-cased mistakenly lowered field

--- monitor.py
+++ monitor.py
@@ -708,7 +708,7 @@ class EDLogs(FileSystemEventHandler):  # type: ignore # See below
                 #     • StationType
                 #     • MarketID
 
-                if entry.get('onstation', False):
+                if entry.get('OnStation', False):
                     self.station = entry.get('StationName', '')
 
                 else:
