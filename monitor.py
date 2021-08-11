0bb06232b88a91cdb829ce7e14efb14af96bf0a3
norohind <60548839+norohind@users.noreply.github.com>
2021-08-11 14:24:27 +0300

    monitor.py: add default value to entry.get('GameMode') in CQC detection

--- monitor.py
+++ monitor.py
@@ -529,7 +529,7 @@ class EDLogs(FileSystemEventHandler):  # type: ignore # See below
                 # Odyssey: bool
                 self.cmdr = entry['Commander']
                 # 'Open', 'Solo', 'Group', or None for CQC (and Training - but no LoadGame event)
-                if not entry.get('Ship') and not entry.get('GameMode') or entry.get('GameMode').lower() == 'cqc':
+                if not entry.get('Ship') and not entry.get('GameMode') or entry.get('GameMode', '').lower() == 'cqc':
                     if 'cqc-loadgame-events' in trace_on:
                         logger.trace(f'loadgame to cqc: {entry}')
 
