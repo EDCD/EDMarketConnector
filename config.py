cbcf5f6d44d143faec2af8f8a4e4559de3dd05ad
Forest <foresto@users.noreply.github.com>
2021-09-17 17:26:22 -0700

    Log git's absence at the INFO level, not ERROR
    The ERROR log level is needlessly high for complaining about git not being installed, especially when we're running from an extracted source archive rather than a repo. Let's keep things calm and avoid polluting desktop session logs. The INFO log level is plenty.

--- config.py
+++ config.py
@@ -117,7 +117,7 @@ def git_shorthash_from_head() -> str:
         out, err = git_cmd.communicate()
 
     except Exception as e:
-        logger.error(f"Couldn't run git command for short hash: {e!r}")
+        logger.info(f"Couldn't run git command for short hash: {e!r}")
 
     else:
         shorthash = out.decode().rstrip('\n')
