2f6132870d8d3c1c1d2f86f9252c5c45b1f898b0
A_D <aunderscored@gmail.com>
2021-10-05 06:58:42 +0200

    added aliases to provider names

---
+++ plugin/provider.py
@@ -0,0 +1,9 @@
+"""Nice aliases for standard providers."""
+
+
+class EDMCProviders:
+    """Provider name aliases."""
+
+    SHIPYARD = 'core.shipyard_url'
+    STATION = 'core.station_url'
+    SYSTEM = 'core.system_url'
