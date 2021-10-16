29c5607400352d830c5a2328f4c45026e5d7627f
A_D <aunderscored@gmail.com>
2021-10-16 13:11:36 +0200

    rename constants for providers

--- plugin/provider.py
+++ plugin/provider.py
@@ -4,6 +4,8 @@
 class EDMCProviders:
     """Provider name aliases."""
 
-    SHIPYARD = 'core.shipyard_url'
-    STATION = 'core.station_url'
-    SYSTEM = 'core.system_url'
+    SHIPYARD_URL = 'core.shipyard_url'
+    STATION_URL = 'core.station_url'
+    STATION_TEXT = 'core.station_text'
+    SYSTEM_URL = 'core.system_url'
+    SYSTEM_TEXT = 'core.system_text'
