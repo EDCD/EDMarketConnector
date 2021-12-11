fc3a7b32a3d6e1d9327b5d9747973df383c1b480
Athanasius <github@miggy.org>
2021-12-11 14:15:30 +0000

    l10n: Make the zh-CN -> zh-Hans comment a HACK

--- l10n.py
+++ l10n.py
@@ -354,8 +354,9 @@ class _Locale:
                 ):
                     languages = wszarray_to_list(buf)
 
-        # OneSky calls "Chinese Simplified" "zh-Hans" in the name of the file,
-        # but that will be zh-CN in terms of locale.  So map zh-CN -> zh-Hans
+        # HACK: <n/a> | 2021-12-11: OneSky calls "Chinese Simplified" "zh-Hans"
+        #    in the name of the file, but that will be zh-CN in terms of
+        #    locale.  So map zh-CN -> zh-Hans
         languages = ['zh-Hans' if lang == 'zh-CN' else lang for lang in languages]
 
         return languages
