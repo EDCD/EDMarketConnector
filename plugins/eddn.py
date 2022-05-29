3702b465e563c75db14ec7a1f60a9173bb5a6b40
Athanasius <github@miggy.org>
2022-05-29 15:03:02 +0100

    eddn: fssdiscoveryscan: Location cross-check paranoia

--- plugins/eddn.py
+++ plugins/eddn.py
@@ -854,6 +854,12 @@ class EDDN:
         #######################################################################
         # Augmentations
         #######################################################################
+        # In this case should add StarPos, but only if the
+        # SystemAddress of where we think we are matches.
+        if this.systemaddress is None or this.systemaddress != entry['SystemAddress']:
+            logger.warning("SystemAddress isn't current location! Can't add augmentations!")
+            return 'Wrong System! Missed jump ?'
+
         ret = this.eddn.entry_augment_system_data(entry, system_name, system_starpos)
         if isinstance(ret, str):
             return ret
