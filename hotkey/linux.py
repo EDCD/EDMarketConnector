470c9b2728d43dfa552050d280e661b0a4c41965
A_D <aunderscored@gmail.com>
2022-12-23 18:02:55 +0200

    hotkey/linux.py: Update stub
    This was missing some abstract methods

--- hotkey/linux.py
+++ hotkey/linux.py
@@ -20,6 +20,37 @@ class LinuxHotKeyMgr(AbstractHotkeyMgr):
         """Unregister the hotkey handling."""
         pass
 
+    def acquire_start(self) -> None:
+        """Start acquiring hotkey state via polling."""
+        pass
+
+    def acquire_stop(self) -> None:
+        """Stop acquiring hotkey state."""
+        pass
+
+    def fromevent(self, event) -> bool | tuple | None:
+        """
+        Return configuration (keycode, modifiers) or None=clear or False=retain previous.
+
+        event.state is a pain - it shows the state of the modifiers *before* a modifier key was pressed.
+        event.state *does* differentiate between left and right Ctrl and Alt and between Return and Enter
+        by putting KF_EXTENDED in bit 18, but RegisterHotKey doesn't differentiate.
+
+        :param event: tk event ?
+        :return: False to retain previous, None to not use, else (keycode, modifiers)
+        """
+        pass
+
+    def display(self, keycode: int, modifiers: int) -> str:
+        """
+        Return displayable form of given hotkey + modifiers.
+
+        :param keycode:
+        :param modifiers:
+        :return: string form
+        """
+        return "Unsupported on linux"
+
     def play_good(self) -> None:
         """Play the 'good' sound."""
         pass
