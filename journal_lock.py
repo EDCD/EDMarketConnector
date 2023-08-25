d20c2818d20e6e10fc4131c234579b155db8aaf7
David Sangrey <rixxan@hullseals.space>
2023-08-25 16:47:34 -0400

    #2051 Revert Unneeded Assignment

--- journal_lock.py
+++ journal_lock.py
@@ -37,8 +37,6 @@ class JournalLock:
 
     def __init__(self) -> None:
         """Initialise where the journal directory and lock file are."""
-        self.retry_popup = None
-        self.journal_dir_lockfile = None
         self.journal_dir: Optional[str] = config.get_str('journaldir') or config.default_journal_dir
         self.journal_dir_path: Optional[pathlib.Path] = None
         self.set_path_from_journaldir()
